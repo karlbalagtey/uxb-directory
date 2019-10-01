24e249cf91444c6c6b111794744d8f853e458f99
Hasan Tareque <hasan@uxblondon.com>
2019-10-01 16:34:41 +0100

    improved code to handle request

--- app.js
+++ app.js
@@ -8,16 +8,26 @@ const userRoutes = require('./routes/userRoutes');
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 // Routes
 app.use(userRoutes);
 
-/**
- * Unspecifed request
- */
-app.use((req, res, next) => {
-    res.status(404).send({
-        'err' : 'Unspecified request'
-    });
+// Error handling 
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
 });
 
 mongoose.connect('mongodb+srv://hmtareque:hasan076@cluster0-mhyrp.mongodb.net/uxb_directory?retryWrites=true&w=majority', { 
